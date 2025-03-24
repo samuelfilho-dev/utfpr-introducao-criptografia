f222a7dc7fe48615a7c2963a5ab813b5078382c5
samuelfilho-dev <snamuelfilho2011@gmail.com>
2025-03-24 19:24:41 -0300

    fix: change algo cipher

--- aula_02/vigenere_cipher.py
+++ aula_02/vigenere_cipher.py
@@ -1,13 +1,3 @@
-import unicodedata
-
-def normalize_text(text):
-    return ''.join(
-        c if c in ALPHABET else unicodedata.normalize('NFD', c).encode('ascii', 'ignore').decode('utf-8')
-        for c in text
-    )
-
-ALPHABET = "abcdefghijklmnopqrstuvwxyz"
-
 def generate_key(msg, key):
     key = list(key)
     if len(msg) == len(key):
@@ -18,37 +8,32 @@ def generate_key(msg, key):
     return "".join(key)
 
 def encrypt_vigenere(msg, key):
-    msg = normalize_text(msg)
-    key = normalize_text(key)
     encrypted_text = []
     key = generate_key(msg, key)
-    
     for i in range(len(msg)):
         char = msg[i]
-        if char in ALPHABET:
-            new_index = (ALPHABET.index(char) + ALPHABET.index(key[i])) % len(ALPHABET)
-            encrypted_text.append(ALPHABET[new_index])
+        if char.isupper():
+            encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('A')) % 26 + ord('A'))
+        elif char.islower():
+            encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('a')) % 26 + ord('a'))
         else:
-            encrypted_text.append('')  # Remove caracteres especiais
-    
+            encrypted_char = char
+        encrypted_text.append(encrypted_char)
     return "".join(encrypted_text)
 
 def decrypt_vigenere(msg, key):
-    msg = normalize_text(msg)
-    key = normalize_text(key)
     decrypted_text = []
     key = generate_key(msg, key)
-    
     for i in range(len(msg)):
         char = msg[i]
-        if char in ALPHABET:
-            new_index = (ALPHABET.index(char) - ALPHABET.index(key[i])) % len(ALPHABET)
-            decrypted_text.append(ALPHABET[new_index])
+        if char.isupper():
+            decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('A'))
+        elif char.islower():
+            decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('a'))
         else:
-            decrypted_text.append('')
-    
+            decrypted_char = char
+        decrypted_text.append(decrypted_char)
     return "".join(decrypted_text)
-
 with open('./aula_02/poema.txt', 'r') as file:
     poema_text = file.read()
 
